8fb79c60b1afbc0a2a03433f71de4592a1719845
Thomas Upton <tom@shipwell.com>
2021-06-30 17:09:45 -0500

    Add theme-ui presets and use the tailwind theme

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,11 @@ import { jsx } from "theme-ui";
 import ReactDOM from "react-dom";
 import Deck from "@mdxp/core";
 import * as components from "@mdxp/components";
+import { tailwind as theme } from "@theme-ui/presets";
 import MDXPresentation from "./presentation.mdx";
 
 ReactDOM.render(
-	<Deck components={components}>
+	<Deck components={components} theme={theme}>
 		<MDXPresentation />
 	</Deck>,
 	document.getElementById("root")
