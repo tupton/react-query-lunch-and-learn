bea9d71d14c9bcc69fe7b76608e33f4e83146806
Thomas Upton <tom@shipwell.com>
2021-06-30 17:24:30 -0500

    Add @theme-ui/prism for code syntax highligting

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,33 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
+import React from "react";
 import ReactDOM from "react-dom";
 import Deck from "@mdxp/core";
 import * as components from "@mdxp/components";
 import { tailwind as theme } from "@theme-ui/presets";
+import Prism from "@theme-ui/prism";
+import syntaxTheme from "@theme-ui/prism/presets/github.json";
 import MDXPresentation from "./presentation.mdx";
 
+const deckTheme = {
+	...theme,
+	styles: {
+		...theme.styles,
+		code: {
+			...syntaxTheme,
+		},
+	},
+};
+
 ReactDOM.render(
-	<Deck components={components} theme={theme}>
+	<Deck
+		components={{
+			...components,
+			pre: ({ children }) => <React.Fragment>{children}</React.Fragment>,
+			code: Prism,
+		}}
+		theme={deckTheme}
+	>
 		<MDXPresentation />
 	</Deck>,
 	document.getElementById("root")
