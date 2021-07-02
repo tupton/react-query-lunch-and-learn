132a487420bcf9104e4c8f5e856e523bf67b0a2f
Thomas Upton <tom@shipwell.com>
2021-07-01 21:56:57 -0500

    Add slides around query functions

---
+++ src/examples/accessorials.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {useQuery} from 'react-query';
+import {Field} from 'formik';
+import {FormikSelect} from 'shipwell-ui';
+import {fetchAccessorialCodesPromise} from 'App/api/shipment'; // highlight-line
+import {filterAccessorials} from 'App/utils/globals';
+
+export const AccessorialField = () => {
+  const getAccessorialsQuery = useQuery('accessorials', () => fetchAccessorialCodesPromise()); // highlight-line
+  const accessorials = filterAccessorials(getAccessorialsQuery.data?.body)?.drayageShipment || [];
+  return (
+    <Field
+      name="accessorials"
+      label="Shipment Accessorials"
+      component={FormikSelect}
+      options={accessorials} // highlight-line
+      getOptionLabel={(option) => option.description}
+      getOptionValue={(option) => option.id}
+      isMulti
+    />
+  );
+};
