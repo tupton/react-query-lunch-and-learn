dec816978b8de43f1652c6f1561d0656c71551b3
Thomas Upton <tom@shipwell.com>
2021-07-01 21:08:40 -0500

    Reusable StepTransition component

--- src/components.js
+++ src/components.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as components from '@mdxp/components';
+import {Step} from '@mdxp/core';
 import Prism from '@theme-ui/prism';
 
 const Pre = ({children}) => <React.Fragment>{children}</React.Fragment>;
@@ -9,8 +10,19 @@ Pre.propTypes = {
   children: PropTypes.node
 };
 
+const StepTransition = (props) => (
+  <Step
+    styles={{
+      before: {opacity: 0},
+      after: {opacity: 1, transition: 'all 0.2s ease-out'}
+    }}
+    {...props}
+  />
+);
+
 export default {
   ...components,
   pre: Pre,
-  code: Prism
+  code: Prism,
+  StepTransition
 };
